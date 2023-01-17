f1e6d12fad3462d1630e93f8aed2e5ec5d6f2ce9
zivo <swift2002999@gmail.com>
2023-01-17 20:47:36 +0500

    Initial commit

---
+++ src/main/java/com/example/exampleweb/ExampleWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exampleweb/ExampleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
